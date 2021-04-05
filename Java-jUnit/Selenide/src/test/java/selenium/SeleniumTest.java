97af4e087b5c13e50418f4f4591e92b3c78f52d0
Oleksii Tymofieiev <oleksiitymofieiev@gmail.com>
2021-04-04 22:22:29 -0700

    0001. Selenide Sample Test.

---
+++ Java-jUnit/Selenide/src/test/java/selenium/SeleniumTest.java
@@ -0,0 +1,28 @@
+package selenium;
+
+import static com.codeborne.selenide.Selenide.*;
+import static com.codeborne.selenide.Condition.*;
+
+import org.junit.Test;
+import org.openqa.selenium.By;
+
+public class SeleniumTest {
+
+    @Test
+    public void firstTest() {
+
+        open("http://explorecalifornia.org/contact.htm");
+        $("#name").should(appear);
+        $("#comments").should(appear);
+
+        $("#name").setValue("oleksiit");
+        $("#state").selectOptionContainingText("California");
+        $("#backpack").click();
+        $(By.name("newsletter")).selectRadio("yes");
+
+        $("#name").shouldHave(value("oleksiit"));
+        assert($("#state").getSelectedText()).equals("California");
+        $("#backpack").shouldBe(selected);
+        $(By.name("newsletter")).shouldHave(value("yes"));
+    }
+}
